fa1f648d988d34b83cdf5b8d49b24ebb816b8b3c
danyaaksenov <den06317520631752>
2021-05-06 12:54:25 +0300

    init

---
+++ src/main/java/com/example/spring/ClassicalMusic.java
@@ -0,0 +1,10 @@
+package com.example.spring;
+
+import org.springframework.stereotype.Component;
+@Component
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/com/example/spring/Computer.java
@@ -0,0 +1,20 @@
+package com.example.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
+public class Computer {
+    private MusicPlayer musicPlayer;
+    private int id;
+
+    @Autowired
+    public Computer(MusicPlayer musicPlayer) {
+        this.musicPlayer = musicPlayer;
+        this.id = 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer " + id + " " + musicPlayer.playMusic();
+    }
+}
